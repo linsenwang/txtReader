174aef0ef2af76467a950d612d0a97422a1d5625
yangqian <1175960101@qq.com>
2025-03-06 12:16:01 +0800

    support folder

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+require('dotenv').config();
 const app = express();
-
-// 指向 py 文件夹的 pro 目录
-const directoryPath = path.resolve('/home/yangqian/Dev/txtReader/pro');
-
 app.use(express.static(__dirname));
+
+// const directoryPath = path.resolve('/home/yangqian/Dev/txtReader/pro');
+const baseDirectory = path.resolve(process.env.PRO_DIR);
+
 
 // 列出 pro 目录下的所有文件，按修改时间排序
 app.get('/list-files', (req, res) => {
+    const requestedDir = req.query.dir || ''; // 获取请求的目录参数
+    console.log('发送的文件列表：', requestedDir);
+    const directoryPath = path.join(baseDirectory, requestedDir); // 拼接路径
+
+    // 确保请求的目录在 baseDirectory 内，防止访问系统其他目录
+    if (!directoryPath.startsWith(baseDirectory)) {
+        return res.status(403).send('访问被拒绝');
+    }
+
     fs.readdir(directoryPath, (err, files) => {
         if (err) {
             console.error('无法扫描目录：', err);
@@ -42,10 +51,15 @@ app.get('/file-content', (req, res) => {
         return res.status(400).send('未提供文件名');
     }
 
-    const sanitizedFileName = path.basename(fileName);
-    const filePath = path.join(directoryPath, sanitizedFileName);
+    console.log('实际文件：', fileName);
+    // const sanitizedFileName = path.basename(fileName);
+    const sanitizedFileName = decodeURIComponent(fileName);
+    const filePath = path.join(baseDirectory, sanitizedFileName);
+    if (!filePath.startsWith(baseDirectory)) {
+        return res.status(400).send('非法路径');
+    }
 
-    // console.log('实际文件路径：', filePath);
+    console.log('实际文件路径：', filePath);
 
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
